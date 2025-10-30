e6a57b3bcb2add752946828fb5331e598d6c7104
Brian Anderson <markba122@gmail.com>
2025-10-30 14:09:18 -0700

    update for brachy pulling

--- DataMiningBrachy/DataMiningBrachy/Program.cs
+++ DataMiningBrachy/DataMiningBrachy/Program.cs
@@ -1,176 +1,20 @@
-﻿using DataBaseStructure;
-using DataBaseStructure.AriaBase;
-using DataWritingTools;
-using DataBaseFileManager;
-
+﻿using DataBaseFileManager;
+using FindingHighModulationPatients;
 namespace DataMiningBrachy
 {
-    public class OutPatient
-    {
-        public string PatientID { get; set; }
-        public int Year { get; set; }
-        public string DateTreated { get; set; }
-        public string TreatmentType { get; set; }
-        public string CourseName { get; set; }
-        public string PlanName { get; set; }
-        public string Type { get; set; }
-        public double Depth { get; set; }
-        public string Notes { get; set; }
-    }
     class Program
     {
-        static List<OutPatient> FindBrachypatients(List<PatientClass> patients)
-        {
-            List<OutPatient> outPatients = new List<OutPatient>();
-            foreach(PatientClass patient in patients)
-            {
-                bool is_cylinder = false;
-                bool added = false;
-                foreach (CourseClass course in patient.Courses)
-                {
-                    if (is_cylinder & added)
-                    {
-                        break;
-                    }
-                    foreach (TreatmentPlanClass planClass in course.TreatmentPlans)
-                    {
-
-                        if (planClass.PlanType == "Brachy")
-                        {
-                            foreach(ApplicatorSetClass applicatorSet in planClass.ApplicatorSets)
-                            {
-                                PrescriptionClass prescription = applicatorSet.Prescription;
-                                if (prescription == null)
-                                {
-                                    continue;
-                                }
-                                foreach (var target in prescription.PrescriptionTargets)
-                                {
-                                    if (target.DosePerFraction != 600 || target.NumberOfFractions != 5)
-                                    {
-                                        continue;
-                                    }
-                                    if (target.Type == "Depth")
-                                    {
-                                        is_cylinder = true;
-                                        OutPatient outPatient = new OutPatient()
-                                        {
-                                            PatientID = patient.MRN,
-                                            Year = planClass.Review.ReviewTime.Year,
-                                            DateTreated = $"{planClass.Review.ReviewTime.Month:D2}" +
-                                                            $"/{planClass.Review.ReviewTime.Day:D2}" +
-                                                            $"/{planClass.Review.ReviewTime.Year}",
-                                            TreatmentType = planClass.PlanType,
-                                            CourseName = course.Name,
-                                            PlanName = planClass.PlanName,
-                                            Type = target.Type,
-                                            Depth = target.Value,
-                                            Notes = prescription.Notes.Replace("\n", " ").Replace("\r", " ")
-                                        };
-                                        added = true;
-                                        outPatients.Add(outPatient);
-                                        break;
-                                    }
-                                }
-                                if (added)
-                                {
-                                    break;
-                                }
-                                else if (applicatorSet.Applicators.Count == 1)
-                                {
-                                    ApplicatorClass applicator = applicatorSet.Applicators.FirstOrDefault();
-                                    if (applicator.Id.ToLower().Contains("cyli"))
-                                    {
-                                        is_cylinder = true;
-                                        OutPatient outPatient = new OutPatient()
-                                        {
-                                            PatientID = patient.MRN,
-                                            Year = planClass.Review.ReviewTime.Year,
-                                            DateTreated = $"{planClass.Review.ReviewTime.Month:D2}" +
-                                                            $"/{planClass.Review.ReviewTime.Day:D2}" +
-                                                            $"/{planClass.Review.ReviewTime.Year}",
-                                            TreatmentType = planClass.PlanType,
-                                            CourseName = course.Name,
-                                            PlanName = planClass.PlanName,
-                                            Type = "",
-                                            Depth = 0,
-                                            Notes = prescription.Notes.Replace("\n", " ").Replace("\r", " ")
-                                        };
-                                        added = true;
-                                        outPatients.Add(outPatient);
-                                    }
-                                }
-                                if (added)
-                                {
-                                    break;
-                                }
-
-                            }
-                        }
-                        if (is_cylinder)
-                        {
-                            break;
-                        }
-                    }
-                }
-            }
-            return outPatients;
-        }
-        static List<PatientClass> FindPatients(List<PatientClass> patients)
-        {
-            List<PatientClass> filteredPatients = new List<PatientClass>();
-            foreach (PatientClass patient in patients)
-            {
-                if (patient.Courses.Count == 0)
-                {
-                    continue;
-                }
-                foreach(CourseClass course in patient.Courses)
-                {
-                    foreach (TreatmentPlanClass treatment in course.TreatmentPlans)
-                    {
-                        if (treatment.Review.ApprovalStatus != "TreatmentApproved")
-                        {
-                            continue;
-                        }
-                        bool addPatient = false;
-                        if (treatment.PlanType.ToLower().Contains("brachy"))
-                        {
-                            if (treatment.PlanName.ToLower().Contains("cylinder") ||
-                                treatment.PlanName.ToLower().Contains("skin") ||
-                                treatment.PlanName.ToLower().Contains("scalp") ||
-                                treatment.PlanName.ToLower().Contains("nose") ||
-                                treatment.PlanName.ToLower().Contains("nasal"))
-                            {
-                                addPatient = true;
-                            }
-                        }
-                        if (addPatient)
-                        {
-                            filteredPatients.Add(patient);
-                            Console.WriteLine($"{patient.MRN}^{course.Name}^{treatment.PlanName}^{treatment.Review.ReviewTime}");
-                        }
-                    }
-                }
-            }
-            return filteredPatients;
-        }
         static void Main(string[] args)
         {
-            //FileSync.SyncFilesParallel(@"\\ad.ucsd.edu\ahs\CANC\RADONC\BMAnderson\DataBases", @"C:\Users\BRA008\Modular_Projects\LocalDatabases", maxDegreeOfParallelism: 8);
-            MainRun.Update();
+            //MainRun.Update();
+            //FileSync.SyncFilesParallel(@"\\ad.ucsd.edu\ahs\CANC\RADONC\BMAnderson\DataBases", @"C:\Users\BRA008\Modular_Projects\LocalDatabases", maxDegreeOfParallelism: -1);
+            //FindModulationPatientsClass findModulationPatients = new FindModulationPatientsClass();
+            //findModulationPatients.Main(args);
             return;
-            string dataDirectory = @"\\ad.ucsd.edu\ahs\CANC\RADONC\BMAnderson\DataBases";
-            List<string> jsonFiles = new List<string>();
-            jsonFiles = AriaDataBaseJsonReader.ReturnPatientFileNames(@"C:\Users\BRA008\Modular_Projects\LocalDatabases\2025", jsonFiles, "*.json", SearchOption.AllDirectories);
-            jsonFiles = AriaDataBaseJsonReader.ReturnPatientFileNames(@"C:\Users\BRA008\Modular_Projects\LocalDatabases\2024", jsonFiles, "*.json", SearchOption.AllDirectories);
-            jsonFiles = AriaDataBaseJsonReader.ReturnPatientFileNames(@"C:\Users\BRA008\Modular_Projects\LocalDatabases\2023", jsonFiles, "*.json", SearchOption.AllDirectories);
-            jsonFiles = AriaDataBaseJsonReader.ReturnPatientFileNames(@"C:\Users\BRA008\Modular_Projects\LocalDatabases\2022", jsonFiles, "*.json", SearchOption.AllDirectories);
-            List<PatientClass> allPatients = new List<PatientClass>();
-            allPatients = AriaDataBaseJsonReader.ReadPatientFiles(jsonFiles);
-            var cylinderPatients = FindBrachypatients(allPatients);
-            string outputCsvPath = Path.Combine(dataDirectory, "BrachyPatients_4cmPlusDepth_2025.csv");
-            CsvTools.WriteToCsv<OutPatient>(cylinderPatients, outputCsvPath);
+            DataMiningCylinder dataMiningCylinder = new DataMiningCylinder();
+            dataMiningCylinder.Main(args);
+            DataMiningSkin dataMiningSkin = new DataMiningSkin();
+            dataMiningSkin.Main(args);
         }
     }
 }
---
+++ DataMiningBrachy/UCSD_FindingBrachySkin/DataMiningSkin.cs
@@ -0,0 +1,118 @@
+﻿using DataBaseStructure;
+using DataBaseStructure.AriaBase;
+using DataWritingTools;
+using System.Collections.Generic;
+using System.IO;
+
+namespace UCSD_FindingBrachySkin
+{
+    public class OutPatient
+    {
+        public string PatientID { get; set; }
+        public int Year { get; set; }
+        public string DateTreated { get; set; }
+        public string TreatmentType { get; set; }
+        public string CourseName { get; set; }
+        public string PlanName { get; set; }
+        public string Type { get; set; }
+        public double Depth { get; set; }
+        public string Notes { get; set; }
+    }
+    class DataMiningSkin
+    {
+        static List<OutPatient> FindBrachySkinPatients(List<PatientClass> patients)
+        {
+            List<OutPatient> outPatients = new List<OutPatient>();
+            foreach (PatientClass patient in patients)
+            {
+                bool is_skin = false;
+                bool added = false;
+                foreach (CourseClass course in patient.Courses)
+                {
+                    if (is_skin & added)
+                    {
+                        break;
+                    }
+                    if (course.Name.ToLower().Contains("gyn") || course.Name.ToLower().Contains("pelv"))
+                    {
+                        continue;
+                    }
+                    foreach (TreatmentPlanClass planClass in course.TreatmentPlans)
+                    {
+                        if (planClass.PlanName.ToLower().Contains("not"))
+                        {
+                            continue;
+                        }
+                        if (planClass.PlanType == "Brachy")
+                        {
+                            foreach (ApplicatorSetClass applicatorSet in planClass.ApplicatorSets)
+                            {
+                                PrescriptionClass prescription = applicatorSet.Prescription;
+                                if (prescription == null)
+                                {
+                                    continue;
+                                }
+                                foreach (var target in prescription.PrescriptionTargets)
+                                {
+                                    if (target.DosePerFraction == 600 && target.NumberOfFractions == 5)
+                                    {
+                                        continue;
+                                    }
+                                    if (target.Type == "Depth")
+                                    {
+                                        is_skin = true;
+                                        OutPatient outPatient = new OutPatient()
+                                        {
+                                            PatientID = patient.MRN,
+                                            Year = planClass.Review.ReviewTime.Year,
+                                            DateTreated = $"{planClass.Review.ReviewTime.Month:D2}" +
+                                                            $"/{planClass.Review.ReviewTime.Day:D2}" +
+                                                            $"/{planClass.Review.ReviewTime.Year}",
+                                            TreatmentType = planClass.PlanType,
+                                            CourseName = course.Name,
+                                            PlanName = planClass.PlanName,
+                                            Type = target.Type,
+                                            Depth = target.Value,
+                                            Notes = prescription.Notes.Replace("\n", " ").Replace("\r", " ")
+                                        };
+                                        added = true;
+                                        outPatients.Add(outPatient);
+                                        break;
+                                    }
+                                }
+                                if (added)
+                                {
+                                    break;
+                                }
+                                if (added)
+                                {
+                                    break;
+                                }
+
+                            }
+                        }
+                        if (is_skin)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            return outPatients;
+        }
+        static void Main(string[] args)
+        {
+            string dataDirectory = @"\\ad.ucsd.edu\ahs\CANC\RADONC\BMAnderson\DataBases";
+            List<string> jsonFiles = new List<string>();
+            jsonFiles = AriaDataBaseJsonReader.ReturnPatientFileNames(@"C:\Users\BRA008\Modular_Projects\LocalDatabases\2025", jsonFiles, "*.json", SearchOption.AllDirectories);
+            jsonFiles = AriaDataBaseJsonReader.ReturnPatientFileNames(@"C:\Users\BRA008\Modular_Projects\LocalDatabases\2024", jsonFiles, "*.json", SearchOption.AllDirectories);
+            jsonFiles = AriaDataBaseJsonReader.ReturnPatientFileNames(@"C:\Users\BRA008\Modular_Projects\LocalDatabases\2023", jsonFiles, "*.json", SearchOption.AllDirectories);
+            //jsonFiles = AriaDataBaseJsonReader.ReturnPatientFileNames(@"C:\Users\BRA008\Modular_Projects\LocalDatabases\2022", jsonFiles, "*.json", SearchOption.AllDirectories);
+            List<PatientClass> allPatients = new List<PatientClass>();
+            allPatients = AriaDataBaseJsonReader.ReadPatientFiles(jsonFiles);
+            var cylinderPatients = FindBrachySkinPatients(allPatients);
+            string outputCsvPath = Path.Combine(dataDirectory, "SkinPatients.csv");
+            CsvTools.WriteToCsv<OutPatient>(cylinderPatients, outputCsvPath);
+        }
+    }
+}
