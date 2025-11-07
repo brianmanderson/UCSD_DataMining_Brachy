e537f5855f99d14db52642eebe1f23d34060c294
Brian Anderson <markba122@gmail.com>
2025-11-07 15:04:52 -0800

    updates

---
+++ DataMiningBrachy/DataMiningBrachy/DataMiningImaging.cs
@@ -0,0 +1,151 @@
+﻿using DataBaseStructure;
+using DataBaseStructure.AriaBase;
+using DataWritingTools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataMiningBrachy
+{
+    public class OutPatientImages
+    {
+        public string PatientID { get; set; }
+        public int Year { get; set; }
+        public string DateTreated { get; set; }
+        public string TreatmentType { get; set; }
+        public string CourseName { get; set; }
+        public string PlanName { get; set; }
+        public string Type { get; set; }
+        public double Depth { get; set; }
+        public string Notes { get; set; }
+        public double DosePerFraction { get; set; }
+        public int NumberOfFractions { get; set; }
+        public override string ToString()
+        {
+            return $"{PatientID} with {PlanName}";
+        }
+    }
+    class DataMiningImaging
+    {
+        static List<OutPatient> FindImagePatients(List<PatientClass> patients)
+        {
+            List<OutPatient> outPatients = new List<OutPatient>();
+            foreach (PatientClass patient in patients)
+            {
+                bool is_cylinder = false;
+                bool added = false;
+                foreach (CourseClass course in patient.Courses)
+                {
+                    if (is_cylinder & added)
+                    {
+                        break;
+                    }
+                    foreach (TreatmentPlanClass planClass in course.TreatmentPlans)
+                    {
+
+                        if (planClass.PlanType == "Brachy")
+                        {
+                            foreach (ApplicatorSetClass applicatorSet in planClass.ApplicatorSets)
+                            {
+                                PrescriptionClass prescription = applicatorSet.Prescription;
+                                if (prescription == null)
+                                {
+                                    continue;
+                                }
+                                foreach (var target in prescription.PrescriptionTargets)
+                                {
+                                    if ((target.DosePerFraction != 600 || target.NumberOfFractions != 5) && (target.DosePerFraction != 700 || target.NumberOfFractions != 3))
+                                    {
+                                        continue;
+                                    }
+                                    if (target.Type == "Depth")
+                                    {
+                                        is_cylinder = true;
+                                        OutPatient outPatient = new OutPatient()
+                                        {
+                                            PatientID = patient.MRN,
+                                            Year = planClass.Review.ReviewTime.Year,
+                                            DateTreated = $"{planClass.Review.ReviewTime.Month:D2}" +
+                                                            $"/{planClass.Review.ReviewTime.Day:D2}" +
+                                                            $"/{planClass.Review.ReviewTime.Year}",
+                                            TreatmentType = planClass.PlanType,
+                                            CourseName = course.Name,
+                                            PlanName = planClass.PlanName,
+                                            Type = target.Type,
+                                            Depth = target.Value,
+                                            Notes = prescription.Notes.Replace("\n", " ").Replace("\r", " "),
+                                            DosePerFraction = target.DosePerFraction,
+                                            NumberOfFractions = target.NumberOfFractions
+                                        };
+                                        added = true;
+                                        outPatients.Add(outPatient);
+                                        break;
+                                    }
+                                }
+                                if (added)
+                                {
+                                    break;
+                                }
+                                else if (applicatorSet.Applicators.Count == 1)
+                                {
+                                    ApplicatorClass applicator = applicatorSet.Applicators.FirstOrDefault();
+                                    if (applicator.Id.ToLower().Contains("cyli"))
+                                    {
+                                        is_cylinder = true;
+                                        OutPatient outPatient = new OutPatient()
+                                        {
+                                            PatientID = patient.MRN,
+                                            Year = planClass.Review.ReviewTime.Year,
+                                            DateTreated = $"{planClass.Review.ReviewTime.Month:D2}" +
+                                                            $"/{planClass.Review.ReviewTime.Day:D2}" +
+                                                            $"/{planClass.Review.ReviewTime.Year}",
+                                            TreatmentType = planClass.PlanType,
+                                            CourseName = course.Name,
+                                            PlanName = planClass.PlanName,
+                                            Type = "",
+                                            Depth = 0,
+                                            Notes = prescription.Notes.Replace("\n", " ").Replace("\r", " "),
+                                            DosePerFraction = applicatorSet.Prescription.PrescriptionTargets[0].DosePerFraction,
+                                            NumberOfFractions = applicatorSet.Prescription.PrescriptionTargets[0].NumberOfFractions
+                                        };
+                                        added = true;
+                                        outPatients.Add(outPatient);
+                                    }
+                                }
+                                if (added)
+                                {
+                                    break;
+                                }
+
+                            }
+                        }
+                        if (is_cylinder)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            return outPatients;
+        }
+        public static void MainImaging()
+        {
+            //FileSync.SyncFilesParallel(@"\\ad.ucsd.edu\ahs\CANC\RADONC\BMAnderson\DataBases", @"C:\Users\BRA008\Modular_Projects\LocalDatabases", maxDegreeOfParallelism: 8);
+            //MainRun.Update();
+            //return;
+            string dataDirectory = @"\\ad.ucsd.edu\ahs\CANC\RADONC\BMAnderson\DataBases";
+            List<string> jsonFiles = new List<string>();
+            jsonFiles = AriaDataBaseJsonReader.ReturnPatientFileNames(@"C:\Users\BRA008\Modular_Projects\LocalDatabases\2025", jsonFiles, "*.json", SearchOption.AllDirectories);
+            //jsonFiles = AriaDataBaseJsonReader.ReturnPatientFileNames(@"C:\Users\BRA008\Modular_Projects\LocalDatabases\2024", jsonFiles, "*.json", SearchOption.AllDirectories);
+            //jsonFiles = AriaDataBaseJsonReader.ReturnPatientFileNames(@"C:\Users\BRA008\Modular_Projects\LocalDatabases\2023", jsonFiles, "*.json", SearchOption.AllDirectories);
+            //jsonFiles = AriaDataBaseJsonReader.ReturnPatientFileNames(@"C:\Users\BRA008\Modular_Projects\LocalDatabases\2022", jsonFiles, "*.json", SearchOption.AllDirectories);
+            List<PatientClass> allPatients = new List<PatientClass>();
+            allPatients = AriaDataBaseJsonReader.ReadPatientFiles(jsonFiles);
+            var cylinderPatients = FindImagePatients(allPatients);
+            string outputCsvPath = Path.Combine(dataDirectory, "ImagePatients.csv");
+            CsvTools.WriteToCsv<OutPatient>(cylinderPatients, outputCsvPath);
+        }
+    }
+}
--- DataMiningBrachy/DataMiningBrachy/Program.cs
+++ DataMiningBrachy/DataMiningBrachy/Program.cs
@@ -1,16 +1,26 @@
 ﻿using DataBaseFileManager;
 using FindingHighModulationPatients;
+using FindingHAPatients;
 namespace DataMiningBrachy
 {
     class Program
     {
+        static void QueryAria()
+        {
+            AriaQueryActivities.AriaToCSV();
+        }
         static void Main(string[] args)
         {
+            DataMiningImaging.MainImaging();
+            //QueryAria();
+            return;
             //MainRun.Update();
             //FileSync.SyncFilesParallel(@"\\ad.ucsd.edu\ahs\CANC\RADONC\BMAnderson\DataBases", @"C:\Users\BRA008\Modular_Projects\LocalDatabases", maxDegreeOfParallelism: -1);
+            //return;
             //FindModulationPatientsClass findModulationPatients = new FindModulationPatientsClass();
             //findModulationPatients.Main(args);
-            DataMiningReTreat.MainRun();
+            FindHAPatientsClass.MainRun();
+            //DataMiningReTreat.MainRun();
             return;
             DataMiningCylinder dataMiningCylinder = new DataMiningCylinder();
             dataMiningCylinder.Main(args);
