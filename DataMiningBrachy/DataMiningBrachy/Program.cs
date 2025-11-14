5fbe393d6f5ff059ec682004c89693ac5cd9cc55
Brian Anderson <markba122@gmail.com>
2025-11-14 12:52:35 -0800

    added HN finder, Imaging, and retreat

--- DataMiningBrachy/DataMiningBrachy/Program.cs
+++ DataMiningBrachy/DataMiningBrachy/Program.cs
@@ -1,18 +1,26 @@
 ﻿using DataBaseFileManager;
 using FindingHighModulationPatients;
 using FindingHAPatients;
+using SendEmailCSharp;
 namespace DataMiningBrachy
 {
     class Program
     {
         static void QueryAria()
         {
-            AriaQueryActivities.AriaToCSV();
+            List<PatientAriaQuery> patients = AriaQueryActivities.QueryAria();
+            string csv_path = ".\\Results.csv";
+            string directoryPath = Path.GetDirectoryName(csv_path);
+            if (string.IsNullOrEmpty(directoryPath))
+                directoryPath = ".";
+            PatientListToCSV(issuePatients, csv_path);
         }
         static void Main(string[] args)
         {
-            DataMiningImaging.MainImaging();
-            //QueryAria();
+            //string[] k = { "BrianAnderson@health.ucsd.edu" };
+            //SendEmailClass.SendEmail("rmanger@health.ucsd.edu", k, "Great email!", "I love it, great job");
+            //DataMiningImaging.MainImaging();
+            QueryAria();
             return;
             //MainRun.Update();
             //FileSync.SyncFilesParallel(@"\\ad.ucsd.edu\ahs\CANC\RADONC\BMAnderson\DataBases", @"C:\Users\BRA008\Modular_Projects\LocalDatabases", maxDegreeOfParallelism: -1);
